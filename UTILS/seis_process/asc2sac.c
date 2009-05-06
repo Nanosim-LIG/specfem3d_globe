c973ab5c572c6ae1db85696adf644040ff1f2bb0
Eh Tan <tan2@earth.sinica.edu.tw>
2009-05-06 18:29:03 +0000

    Add license and usage

--- UTILS/seis_process/asc2sac.c
+++ UTILS/seis_process/asc2sac.c
@@ -1,4 +1,22 @@
-/* License
+/*
+ * asc2sac.c by Eh Tan
+ *
+ * Copyright (C) 2009, California Institute of Technology.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 #include <stdio.h>
@@ -20,23 +38,30 @@ main(int argc, char *argv[])
     FILE *f;
 
     if(argc < 4) {
-        fprintf(stderr, "%s ascii-file npts sac-file\n", argv[0]);
+        fprintf(stderr,
+                "Converting evenly spaced time series ASCII file to SAC binary\n"
+                "usage: %s ascii-file npts sac-file\n\n"
+                "ascii-file: name of the input file. The file must have two columns,\n"
+                "            1st column must be evenly spaced.\n"
+                "npts:       number of lines in the input file.\n"
+                "sac-file:   name of the output file.\n",
+                argv[0]);
         exit(1);
     }
 
     str = argv[2];
     errno = 0;
+    endptr = str;
     npts = strtol(str, &endptr, 10);
 
     /* Check for various possible errors */
-    if ((errno == ERANGE && (npts == INT_MAX || npts == INT_MIN))
-        || (errno != 0 && npts <= 0)) {
-        perror("strtol");
-        exit(EXIT_FAILURE);
+    if (errno || (*endptr != 0)) {
+        fprintf(stderr, "Error when converting '%s' to integer\n", str);
+        exit(-1);
     }
 
-    if (endptr == str) {
-        fprintf(stderr, "No npts were found\n");
+    if (npts <= 0) {
+        fprintf(stderr, "npts must be positive\n");
         exit(EXIT_FAILURE);
     }
 
