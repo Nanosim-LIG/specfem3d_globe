c864f56e9bd1ad8e3a06384a4a82ae600cff5a6c
Kevin Pouget <kevin.pouget@imag.fr>
2015-03-16 10:11:19 +0100

    work on launch script

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+# /bin/bash
 
-SPECFEM_BUILD=/home/kpouget/specfem3d_build
-SPECFEM_DIR=/home/kpouget/specfem3d_globe
+SPECFEM_BUILD=$HOME/specfem3d_build
+SPECFEM_DIR=$HOME/specfem3d_globe
 SPECFEM_DIR_ABS=$(readlink -f $SPECFEM_DIR)
 
 EXAMPLE_DIR=$SPECFEM_DIR_ABS/EXAMPLES/regional_Greece_small/DATA
@@ -12,7 +12,7 @@ EXAMPLE_DIR=$SPECFEM_DIR_ABS/EXAMPLES/regional_Greece_small/DATA
 cd $SPECFEM_BUILD
 WORKDIR=$(pwd)
 
-echo "Example directory: $EXAMPLE_DIR"
+#echo "Example directory: $EXAMPLE_DIR"
 
 get_config() {
     CONF="DATA/Par_file"
@@ -64,25 +64,29 @@ adjust() {
     done
 }
 
-SPEC_RUN=/home/kpouget/specfem3d_run
-SPEC_BUILD=/home/kpouget/specfem3d_build
+SPEC_RUN=$HOME/specfem3d_run
+SPEC_BUILD=$HOME/specfem3d_build
 
 specfem() {
     get_config
     echo "Preparing specfem script on $NUM_NODES processors." | grep --color=always -e '^Preparing.*$' >&2
+    if [[ -z "$1" ]]
+    then
+        JOB_NAME=specfem
+    else
+        JOB_NAME=$1
+    fi
     
-    JOB_NAME=specfem3d
-    
-    cat > job_specfem.sh <<EOF
+    cat > job_$JOB_NAME.sh <<EOF
 #!/bin/bash
 
 #SBATCH --partition=mb
 #SBATCH --ntasks=$NUM_NODES
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=$JOB_NAME
-#SBATCH --error=$SPEC_RUN/%j/$JOB_NAME.err
-#SBATCH --output=$SPEC_RUN/%j/$JOB_NAME.out
+#SBATCH --error=$SPEC_RUN/%j/log_$JOB_NAME.err
+#SBATCH --output=$SPEC_RUN/%j/log_$JOB_NAME.out
 #SBATCH --workdir=$WORKDIR
 #SBATCH --gres=gpu
 
@@ -117,11 +121,22 @@ fi
 ### Select execution type ###
 #############################
 
+
 run_mesh=\$([[ ! -f .mesh_created || DATA/Par_file -nt .mesh_created || ! -f DATABASES_MPI/addressing.txt ]])\$?
 
 what=\$([[ \$run_mesh == 0 ]] && echo meshfem || echo specfem)
 
 trap cleanup EXIT
+
+if [[ ! "$JOB_NAME" -eq "\$what" ]]
+then
+  echo "Cannot run $JOB_NAME. Execution CANCELLED"
+  [[ ! -f .mesh_created ]] && echo "File .mesh_created not found" 
+  [[ DATA/Par_file -nt .mesh_created ]] && echo "DATA/Par_file newer than .mesh_created"
+  [[ ! -f DATABASES_MPI/addressing.txt ]] && echo "File DATABASES_MPI/addressing.txt not found"
+
+  exit 1
+fi
 
 #########################
 ### Trigger execution ###
@@ -163,9 +178,8 @@ fi
 exit \$how
 
 EOF
-    rm batch/$JOB_NAME.{err,out} -f
     
-    echo "job_specfem.sh"
+    echo "job_$JOB_NAME.sh"
 }
 
 enqueue() {
@@ -182,7 +196,91 @@ enqueue() {
         echo "Job $JOB depends of $DEPENDS." >&2
     fi
     chmod u+x $JOB
-    sbatch $OPT $JOB
+    my_sbatch $OPT $JOB
+}
+REMOTE="ssh.hca.bsc.es"
+REMOTE="localhost"
+compile() {
+    if ssh $REMOTE "bash -c 'source ~/.profile ; \
+                      cd ~/specfem3d_build && \
+                      touch DATA/Par_file && \
+                      make -j12'" &> $SPEC_RUN/specfem3d_compile.log
+    then
+        echo "Make successed"
+        return 0
+    else
+        echo "Make failed ... :'( Check $SPEC_RUN/specfem3d_compile.log"
+	return 1
+    fi
+}
+
+run_benchmark() {    
+    echo "Prepare mesher for $1"
+    # run mesher
+    if ! compile
+    then
+        echo "Couldn't compile, abort mesher run for $1"
+        return 1
+    fi
+    echo "Run mesher for $1"
+    run_example.sh go wait silent
+	
+    OUT="$(run_example.sh see |& grep 'CANCELLED')"
+    if [[ ! -z "$OUT" ]]
+    then
+	echo "Execution for $1 cancelled ..."
+	echo "$OUT"
+	return 1
+    fi
+    # run mesher
+    echo "Prepare specfem for $1"
+    if ! compile
+    then
+        echo "Couldn't compile, abort specfem run for $1"
+        return 1
+    fi
+    
+    echo "Run specfem for $1"
+    touch .mesh_created
+    run_example.sh go wait silent
+    
+    echo "Done for $1"
+
+    return 0
+}
+
+benchmark() {
+    if [[ -e ~/specfem3d_run && ! -z "$(ls -A ~/specfem3d_run)" ]]
+    then
+        echo "Old run dir backup"
+        mv ~/specfem3d_run{,.$(date --iso-8601=minutes)}
+    fi
+    
+    mkdir ~/specfem3d_run -p
+    cd $SPECFEM_BUILD
+    
+	exec > >(tee $SPEC_RUN/specfem3d_benchmark.log)
+    exec 2>&1
+
+    NEX=$(echo $(grep NEX DATA/Par_file | head -1 | cut -d= -f2))a
+
+	# comment to compute dynamically
+	NPROC_VALUES="4 8 16 32"
+	if [[ -z $NPROC_VALUES ]]
+	then
+		echo "Computing NPROC values with DATA/Par_File::NEX=$NEX"
+    	NPROC_VALUES=$(specfem_nproc_per_nex $NEX)
+	fi
+	echo "Benchmarking specfem for NPROC=$NPROC_VALUES"
+
+    for val in $NPROC_VALUES
+    do
+        echo "Run benchmark with NPROC=$val"
+        sed "s|^\(NPROC_XI\s*=\).*$|\1 $val|" -i DATA/Par_file
+        sed "s|^\(NPROC_ETA\s*=\).*$|\1 $val|" -i DATA/Par_file
+        
+        run_benchmark $val
+    done
 }
 
 txtblk='\e[0m' # Default
@@ -196,6 +294,9 @@ do
   elif [ $1 == 'adjust' ]
   then
       adjust
+  elif [ $1 == 'benchmark' ]
+  then
+      benchmark
   elif [ $1 == 'specfem' ]
   then
       specfem
@@ -209,16 +310,25 @@ do
       
   elif [ $1 == 'wait' ]
   then
+      if [ "$2" == 'silent' ]
+      then
+          SILENT=1
+          echo "Let's wait in silence ..."
+          shift
+      fi
       cnt=0
-      [[ -e current ]] && file current
-      while [[ -e current ]]
+	  tid=$(readlink current)
+	  tid=${tid##*/}
+	  echo "Waiting for task $tid ..."
+      while squeue | grep -q $tid
       do
-          echo -en "$(squeue -h | grep kpouget) ($cnt s)\r"
+          last="$(squeue -h | grep kpouget | sed 's/\[.*\]/[...]/' | sed 's/ \+ / /g') ($cnt s)"
+          [[ -z $SILENT ]] && echo -en "$last\r"
           sleep 1
           cnt=$((cnt + 1))
       done
+      echo $last # reprint last message for logs, previous one got erased
       
-          
   elif [ $1 == 'go' ]
   then
       run_mesh=$([[ ! -f .mesh_created || DATA/Par_file -nt .mesh_created || ! -f DATABASES_MPI/addressing.txt ]])$?
@@ -234,9 +344,10 @@ do
           exit 1
       fi
       
-      enqueue $(specfem)
+      enqueue $(specfem $what)
   else
       echo "Argument $1 not recognized"
+      exit 1
   fi
   shift
 done
